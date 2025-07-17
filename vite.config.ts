a2b05dca41587a5ce1f96ae474646d76740545d2
Jefson Jacob <jfsnjacob@gmail.com>
2025-07-17 14:52:29 +0530

    .gitignore fix

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  // Remove or comment out the base URL
-  // base: '/crypto-tracker/',
-  plugins: [react()],
-});
